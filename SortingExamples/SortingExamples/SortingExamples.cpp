92b4cf327e2788f2612ddfc212104acd9d131e33
prakashchandrasekaran <techianprakash@gmail.com>
2016-12-17 19:42:01 -0800

    Insertion Sorting

--- SortingExamples/SortingExamples/SortingExamples.cpp
+++ SortingExamples/SortingExamples/SortingExamples.cpp
@@ -2,10 +2,52 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
 
+using namespace std;
+
+void print(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void insertion_sort(int arr[], int size)
+{
+	if (arr == nullptr)
+	{
+		return;
+	}
+
+	if (size <= 1)
+	{
+		return;
+	}
+	
+	int value = -1;
+	int j = -1;
+	for (int i = 1; i < size; i++)
+	{
+		j = i - 1;
+		value = arr[i];
+		while (j >= 0 && arr[j] > value)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = value;
+	}
+}
 
 int main()
 {
-    return 0;
+	int arr[] = { 1,4,5,3,2,5,8,6,2,7,0,9,3 };
+	int size = 13;
+	print(arr, size);
+	insertion_sort(arr, size);
+	print(arr, size);
+	getchar();
 }
-
